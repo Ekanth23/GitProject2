b1ea949010574e6916cea6935143d26ceea75ece
ekanth23 <ekanthn@gmail.com>
2023-11-26 19:53:49 +0530

    three method are included

---
+++ src/test/java/org/git/ClassA.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class ClassA {
+	
+	private void method1() {
+		System.out.println("Method1");
+	}
+	
+	private void method2() {
+		System.out.println("Method2");
+	}
+	
+	private void method3() {
+		System.out.println("Method3");
+	}
+
+}
